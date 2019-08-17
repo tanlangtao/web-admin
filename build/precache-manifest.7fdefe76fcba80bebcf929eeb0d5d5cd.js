c71a49db31e004f7336d791aaa388fd459d235b6
javier <wjavier3434@gmail.com>
2019-08-17 16:40:18 +0800

    build version

---
+++ build/precache-manifest.7fdefe76fcba80bebcf929eeb0d5d5cd.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee6ab27c448141cdd06b99876bf9900c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "13438282acee74dfe23a",
+    "url": "/static/css/2.1b7eb529.chunk.css"
+  },
+  {
+    "revision": "40992353bf68ba15d42a",
+    "url": "/static/css/main.cde40729.chunk.css"
+  },
+  {
+    "revision": "13438282acee74dfe23a",
+    "url": "/static/js/2.b5a8f509.chunk.js"
+  },
+  {
+    "revision": "40992353bf68ba15d42a",
+    "url": "/static/js/main.a4cf60e1.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "9161e238b798c444b732dbbb7420a09e",
+    "url": "/static/media/404.9161e238.png"
+  },
+  {
+    "revision": "ab45504baf89be7c7792b4426c4a6594",
+    "url": "/static/media/bg.ab45504b.jpg"
+  }
+]);
