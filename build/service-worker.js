aea38398277efaf1b30e7d35bab0c94a13e8cf7b
roland <roland@gamstudio.net>
2019-11-05 11:59:07 +0800

    fix

--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0473f9222550bbaa3331ed7d996431ff.js"
+  "/precache-manifest.8708594a3ac2156122d312143d6bbc7d.js"
 );
 
 self.addEventListener('message', (event) => {
