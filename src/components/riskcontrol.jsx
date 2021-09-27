95087f6c4868f8cdef3850259f28757143d1b557
howard <howard.h@yappap.com>
2021-09-27 11:44:26 +0800

    新增每日免费礼金增加金币数据

--- src/components/riskcontrol.jsx
+++ src/components/riskcontrol.jsx
@@ -272,7 +272,10 @@ export default async record => {
 						新增有效代理排行榜增加金币:
 						{res.data["新增有效代理排行榜增加金币"] ? res.data["新增有效代理排行榜增加金币"].totalgold : "-"}
 					</div>
-					
+					<div>
+						每日免费礼金增加金币:
+						{res.data["每日免费礼金增加金币"] ? res.data["每日免费礼金增加金币"].totalgold : "-"}
+					</div>
 					{/* <div>
 					每周佣金奖励领取增加金币：
                         {res.data["每周佣金奖励领取增加金币"]
